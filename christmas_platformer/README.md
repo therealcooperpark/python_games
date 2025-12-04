ab742096875ff34fffe7b65125d67c1c545d4cee
therealcooperpark <therealcooperpark@gmail.com>
2025-12-03 19:25:12 -0500

    Remove christmas platformer

--- christmas_platformer/README.md
+++
@@ -1,3 +0,0 @@
-# Christmas Platformer (placeholder)
-
-This repo stores Cooper's dream video game design for a Christmas-based metroidvania style platformer
--- christmas_platformer/assets/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Assets module for Christmas platformer game
-"""
-
-from .player import Player
-from .simple_platform import SimplePlatform
-
-__all__ = ['Player', 'SimplePlatform']
